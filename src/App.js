adf58397febc4f8d834a18f7f6ddddcc4e5eb28f
Bryan Wegman <xxbw@icloud.com>
2017-03-25 16:38:24 -0700

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+//styles
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Into the Dungeon…</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
