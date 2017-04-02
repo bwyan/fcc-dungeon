3d74b2695b751038a43eadbfc23ae0ec62a1e0b9
Bryan Wegman <xxbw@icloud.com>
2017-04-02 14:25:54 -0700

    Add new weapons.

--- src/data/tiles.js
+++ src/data/tiles.js
@@ -27,15 +27,15 @@ class Item {
 	}
 }
 
-
-
 const tiles = {
 		'wall': new Scenery('wall', 'barrier', '🚧'),
 		'floor': new Scenery('floor', 'pathway', ''),
 		'clown': new Enemy('clown', 'enemy', '🤡', 3, {xp: 3, health: 1}),
 		'potato': new Item('potato', '🥔', {xp: 0, health: 2}),
-		'package': new Item('package', '📦', {xp: 10, health: 5, weapon: 'partyhorn'}),
-		'bat': weapons.bat
+		'package': new Item('package', '📦', {xp: 10, health: 5, weapon: 'umbrella'}),
+		'bat': weapons.bat,
+		'partyhorn': weapons.partyhorn,
+		'umbrella': weapons.umbrella
 }
 
 export default tiles;
