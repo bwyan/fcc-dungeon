c1a8da16131c58f1b0fa629835d8330379b6edc5
Bryan Wegman <xxbw@icloud.com>
2017-04-02 14:25:29 -0700

    Adjust level 1 map to handle more test cases.

--- src/data/maps.js
+++ src/data/maps.js
@@ -4,11 +4,11 @@ const maps = {
     columns: 7,
     tileMap: [
       {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'floor'}, {name: 'potato'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
       {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'potato'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'partyhorn'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
       {name: 'wall'}, {name: 'floor'}, {name: 'clown'}, {name: 'floor'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'package'}, {name: 'floor'}, {name: 'floor'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'package'}, {name: 'floor'}, {name: 'bat'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
       {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}
     ],
     startingPosition: [1, 1]
