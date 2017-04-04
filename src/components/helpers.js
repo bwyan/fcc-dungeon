debc367bd09b758defdb02f355a92568b076e3f0
Bryan Wegman <xxbw@icloud.com>
2017-04-03 20:01:54 -0700

    Add helpers.js to store game rules and UI functions.

---
+++ src/components/helpers.js
@@ -0,0 +1,16 @@
+const helpers = {
+  changePlayerHealth(amount) {
+      let player = this.state.player;
+      const newHealth = player.health += amount;
+
+      if (newHealth > player.maxHealth) {
+        player.health = player.maxHealth;
+      } else {
+        player.health = newHealth;
+      }
+
+      this.setState({player});
+    }
+}
+
+export default helpers;
