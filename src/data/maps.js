a711b1d10ed7102dc4c53468db31d3322539bcd6
Bryan Wegman <xxbw@icloud.com>
2017-04-19 20:03:10 -0700

    Enlarge and improve Map 2.

--- src/data/maps.js
+++ src/data/maps.js
@@ -14,16 +14,19 @@ const maps = {
     startingPosition: [1, 1]
   },
   'm2': {
-    rows: 7,
-    columns: 7,
+    rows: 10,
+    columns: 10,
     tileMap: [
-      {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'floor'}, {name: 'potato'}, {name: 'wall'}, {name: 'floor'}, {name: 'goblin'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'partyhorn'}, {name: 'goblin'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'floor'}, {name: 'clown'}, {name: 'floor'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'package'}, {name: 'floor'}, {name: 'bat'}, {name: 'floor'}, {name: 'door'}, {name: 'wall'},
-      {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}
+      {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'wall'}, {name: 'floor'}, {name: 'wall'}, {name: 'potato'}, {name: 'wall'}, {name: 'floor'}, {name: 'goblin'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'partyhorn'}, {name: 'goblin'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'door'}, {name: 'clown'}, {name: 'clown'}, {name: 'floor'}, {name: 'clown'}, {name: 'floor'}, {name: 'floor'}, {name: 'potato'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'package'}, {name: 'floor'}, {name: 'bat'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'potato'}, {name: 'floor'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'floor'}, {name: 'wall'}, {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'floor'}, {name: 'floor'}, {name: 'floor'}, {name: 'floor'}, {name: 'wall'}, {name: 'partyhorn'}, {name: 'goblin'}, {name: 'floor'}, {name: 'wall'},
+      {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}, {name: 'wall'}
     ],
     startingPosition: [2, 5]
   }
